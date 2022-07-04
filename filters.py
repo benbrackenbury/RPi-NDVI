c21c5f9743e7b4ebbd6c2f94dbe60d38c291fdf1
Ben Brackenbury <benbrack22@icloud.com>
2022-07-04 15:51:47 +0100

    split Application class into more methods

--- filters.py
+++ filters.py
@@ -24,35 +24,6 @@ class Filters:
         bottom[bottom==0] - 0.01
         ndvi = (r.astype(float) - b) / bottom
         return ndvi
-        #start video capture
-        vid = cv2.VideoCapture(0)
-        
-        while(True):
-            #get frame
-            ret, frame = vid.read()
-            #display frame
-            cv2.imshow('Original', frame)
-
-            #increase contrast
-            contrasted = self.contrast_stretch(frame)
-            # cv2.imshow('Contrasted', contrasted)
-
-            #NDVI
-            ndvi = self.calc_ndvi(contrasted)
-            # cv2.imshow('NDVI', ndvi)
-
-            #Contrasted NDVI
-            ndvi_contrasted = self.contrast_stretch(ndvi)
-            # cv2.imshow('NDVI Contrasted', ndvi_contrasted)
-
-            #Colour map
-            color_mapped_prep = ndvi_contrasted.astype(np.uint8)
-            color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
-            cv2.imshow('Colour Mapped', color_mapped_image)
-
-            #exit when q is pressed
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
 
     @staticmethod
     def color_map(image):
--- stream.py
+++ stream.py
@@ -4,39 +4,41 @@ import numpy as np
 from filters import Filters
 
 class Application:
+    videoStream = cv2.VideoCapture(0)
+    original = contrasted = ndvi = ndvi_contrasted = color_mapped = 0
+
+    def end(self):
+        self.videoStream.release()
+        cv2.destroyAllWindows()
+
+    def getFrameAndApplyFilters(self):
+        #get frame from stream
+        ret, frame = self.videoStream.read()
+        self.original = frame
+
+        #filters
+        self.contrasted = Filters.contrast_stretch(self.original)
+        self.ndvi = Filters.calc_ndvi(self.contrasted)
+        self.ndvi_contrasted = Filters.contrast_stretch(self.ndvi)
+        self.color_mapped = Filters.color_map(self.ndvi_contrasted)
+
+    def displayFrame(self):
+        cv2.imshow('Original', self.original)
+        # cv2.imshow('Contrasted', contrasted)
+        # cv2.imshow('NDVI', ndvi)
+        # cv2.imshow('NDVI Contrasted', ndvi_contrasted)
+        cv2.imshow('Colour Mapped', self.color_mapped)
+
     def run(self):
-        #start video capture
-        vid = cv2.VideoCapture(0)
-        
         while(True):
-            #get frame
-            ret, frame = vid.read()
-            #display frame
-            cv2.imshow('Original', frame)
-
-            #increase contrast
-            contrasted = Filters.contrast_stretch(frame)
-            # cv2.imshow('Contrasted', contrasted)
-
-            #NDVI
-            ndvi = Filters.calc_ndvi(contrasted)
-            # cv2.imshow('NDVI', ndvi)
-
-            #Contrasted NDVI
-            ndvi_contrasted = Filters.contrast_stretch(ndvi)
-            # cv2.imshow('NDVI Contrasted', ndvi_contrasted)
-
-            #Colour map
-            color_mapped = Filters.color_map(ndvi_contrasted)
-            cv2.imshow('Colour Mapped', color_mapped)
+            self.getFrameAndApplyFilters()
+            self.displayFrame()
 
             #exit when q is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         
-        #stop capute and kill all windows
-        vid.release()
-        cv2.destroyAllWindows()
 
 app = Application()
-app.run()
+app.run() #runs until q key is pressed
+app.end()
