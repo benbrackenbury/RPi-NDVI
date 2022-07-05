dc75b5ae4ff5dad6ffb1f8a5780f83c33ddeecf1
Ben Brackenbury <benbrack22@icloud.com>
2022-07-05 18:17:50 +0100

    end of day state

--- src/preview.py
+++ src/preview.py
@@ -1,12 +1,18 @@
 import cv2
-import numpy as np
-# from flask import Flask, render_template, Response, stream_with_context, request
-
+import numpy as numpy
 from filters import Filters
+import sys
 
 class Application:
     videoStream = cv2.VideoCapture(0)
     original = contrasted = ndvi = ndvi_contrasted = color_mapped = 0
+
+    def __init__(self):
+        if len(sys.argv) == 2 and sys.argv[1] == "--all":
+            self.show_all = True
+        else:
+            self.show_all = False
+
     def end(self):
         self.videoStream.release()
         cv2.destroyAllWindows()
@@ -24,9 +30,10 @@ class Application:
 
     def displayFrame(self):
         cv2.imshow('Original', self.original)
-        # cv2.imshow('Contrasted', contrasted)
-        # cv2.imshow('NDVI', ndvi)
-        # cv2.imshow('NDVI Contrasted', ndvi_contrasted)
+        if self.show_all:
+            cv2.imshow('Contrasted', self.contrasted)
+            cv2.imshow('NDVI', self.ndvi)
+            cv2.imshow('NDVI Contrasted', self.ndvi_contrasted)
         cv2.imshow('Colour Mapped', self.color_mapped)
 
     def run(self):
--- src/webview.py
+++ src/webview.py
@@ -3,12 +3,14 @@ import cv2
 from filters import Filters
 
 app = Flask('__name__')
-camera = cv2.VideoCapture(-1)
 
-def gen_frames(selected_filter):
+def gen_frames():
+    camera = cv2.VideoCapture(-1)
     while True:
         success, frame = camera.read()
         if success == False:
+            camera.release()
+            print("Please refresh page")
             break
         else:
             original = frame
@@ -23,48 +25,20 @@ def gen_frames(selected_filter):
             _, ndvi_contrasted_buffer = cv2.imencode('.jpg', ndvi_contrasted)
             _, color_mapped_buffer = cv2.imencode('.jpg', color_mapped)
 
-            if selected_filter == 'original':
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + original_buffer.tobytes() + b'\r\n')
-            elif selected_filter == 'contrasted':
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + contrasted_buffer.tobytes() + b'\r\n')
-            elif selected_filter == 'ndvi':
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + ndvi_buffer.tobytes() + b'\r\n')
-            elif selected_filter == 'ndvi_contrasted':
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + ndvi_contrasted_buffer.tobytes() + b'\r\n')
-            elif selected_filter == 'color_mapped':
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + color_mapped_buffer.tobytes() + b'\r\n')
-            else:
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + original_buffer.tobytes() + b'\r\n')
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + color_mapped_buffer.tobytes() + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('feed.html')
 
+@app.route('/original')
+def original():
+    return render_template('original.html')
+
 @app.route('/video')
 def video():
-    return Response(gen_frames('original'), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/contrasted')
-def contrasted():
-    return Response(gen_frames('contrasted'), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/ndvi')
-def ndvi():
-    return Response(gen_frames('ndvi'), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/ndvi_contrasted')
-def ndvi_contrasted():
-    return Response(gen_frames('ndvi_contrasted'), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/color_map')
-def color_map():
-    return Response(gen_frames('color_mapped'), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(port='3000', host='0.0.0.0', debug='true')
